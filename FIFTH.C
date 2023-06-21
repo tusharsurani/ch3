297de638d3cac4c55af85643775ba9777cd6b7c4
tusharsurani <137286868+tusharsurani@users.noreply.github.com>
2023-06-21 16:46:31 +0530

    Add files via upload

---
+++ FIFTH.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+  void main()
+  {
+
+    int l=90,area;
+    clrscr();
+    area=l*l;
+    printf("area of squer l*l=%d",area);
+    getch();
+  }
---
+++ SIXTH.C
@@ -0,0 +1,15 @@
+  #include<stdio.h>
+  #include<conio.h>
+  void main()
+  {
+  float l,area,b;
+  clrscr();
+  printf("enter value L=");
+  scanf("%f",&l);
+  printf("enter value B=");
+  scanf("%f",&b);
+  area=l*b;
+  printf("area ofsquer l*b=%.2f",area);
+
+  getch();
+  }
